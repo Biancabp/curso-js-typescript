80f8f978ff5622253d9891b199da6b735e549409
Bianca Bezerra Pires <58571807+Biancabp@users.noreply.github.com>
2022-03-25 22:23:40 -0300

    Add files via upload

---
+++ variaveis/index.js
@@ -0,0 +1,8 @@
+const nome = 'bianca' + '.';
+const idade = 16;
+const altura = 158;
+const peso = 53;
+let imc = peso / (altura * altura);
+let anoNascimento = 2022 - idade;
+
+console.log("oi,", nome, "Voce tem", idade, "anos,", altura, "metros,", "seu indice de massa corporal eh", imc, "e seu ano de nascimento eh", anoNascimento)
